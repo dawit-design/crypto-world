c1aaec642be157af7dff02f76665d2f844837fd7
dawit-design <dawitawegchow@gmail.com>
2022-02-22 19:14:35 -0500

    rendered data and created table

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,33 +1,49 @@
 // import {Link} from "react-router-dom";
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { useTable, useSortBy } from 'react-table';
+import { Table} from 'react-bootstrap';
 import {api} from '../services/index'
 
 function Coin(){
-    const [currencies, setCurrencies] = useState('')
+    const [currencies, setCurrencies] = useState([])
    
-
   useEffect(() => {
-    const getCoins = async () => {
+      const getCoins = async () => {
       const response = await axios.get(api)
       console.log(response.data[0])
       setCurrencies(response.data)
     }
     getCoins()
   },[])
-  
     if(!currencies) return null
     
     return (
         <div className = "coin-list">
             {currencies.map((currency) => (
                 <div key={currency.id} className="coins"> 
-                 <img src={currency.image}/>
+                 {/* <img src={currency.image}/>
                  <h2>{currency.name}</h2>
                  <h3>{currency.symbol}</h3>
-                 <h3>$ {currency.current_price} </h3>
-                 <h3>{currency.market_cap_change_percentage_24h.toFixed(2)} %</h3>
+                 
+                  */}
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Change</th>
+                        <th>Marekt Cap</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td><img src={currency.image}/> - <h2>{currency.name}- {currency.symbol}</h2> </td>
+                        <td><h3>$ {currency.current_price} </h3></td>
+                        <td><h3>$ {currency.market_cap_change_24h} </h3></td>
+                        <td><h3>{currency.market_cap_change_percentage_24h.toFixed(2)} %</h3></td>
+                        </tr>
+                    </tbody>
+        </Table>
                 </div>
             ))}
         </div>
