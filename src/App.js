e5583c0bd5bc6053c2d9b5c3be42b41abe51b20e
dawit-design <dawitawegchow@gmail.com>
2022-02-25 11:24:30 -0500

    removed some components

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   return (
     <div className="App">
        <div class="Crypto List">
-          {/* <Coin /> */}
-          <Trending />
+          {/* <Trending /> */}
+          <Coin />
        {/* <Nav />
         <Route exact path="/">  */}
         {/* </Route> */}
